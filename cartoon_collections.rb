89875fc3e22cc378e39b0d172b7731ee1f3d8477
Winnie Mendoza <wrm956@gmail.com>
2022-08-08 16:36:42 -0400

    commit tree aa72ce111ce132e4b4474324686d9032186d3194

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,22 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.map.with_index(1) { |name, index| puts "#{index}. #{name}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteers)
+  planeteers.map { |element| "#{element.capitalize()}!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? { |call| call.length > 4 }
+
+  # call_truthiness = calls.map do |call| 
+  #   call.length > 4 ? true : false
+  # end
+  # return call_truthiness.include?(true) ? true : false
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(foods)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheeses = foods & cheese_types
+  cheeses.empty? ? nil : cheeses.first
 end
